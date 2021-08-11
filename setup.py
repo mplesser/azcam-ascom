15ad103c2e003b118adea3ac504ad9ac061a01dd
Michael Lesser <mlesser@arizona.edu>
2021-08-11 13:11:15 -0700

    clean repo

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="azcam-ascom",
+    version="21.0.1",
+    description="azcam extension for ASCOM cameras",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Michael Lesser",
+    author_email="mlesser@arizona.edu",
+    keywords="",
+    packages=find_packages(),
+    zip_safe=False,
+    url="https://mplesser.github.io/azcam/",
+    install_requires=["azcam"],
+)
